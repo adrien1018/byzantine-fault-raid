cec613d22fe9a33ce491210a991123facbf02708
adrien1018 <adrien1018@users.noreply.github.com>
2024-06-03 16:37:56 -0700

    Add server run script

---
+++ run_servers.sh
@@ -0,0 +1,6 @@
+rm -rf s0 s1 s2 s3
+./filesys_server --port 8080 --index 0 -s s0 &
+./filesys_server --port 8081 --index 1 -s s1 &
+./filesys_server --port 8082 --index 2 -s s2 &
+./filesys_server --port 8083 --index 3 -s s3 &
+wait $(jobs -p)
