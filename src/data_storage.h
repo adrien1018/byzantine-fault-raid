8c6a481ae7e7bb6a622af491d3e60cd5cd121e8b
Joe Tsai <joemhtsai@gmail.com>
2024-06-06 22:45:03 -0700

    Remove unused server variable

--- src/data_storage.h
+++ src/data_storage.h
@@ -1,8 +1,8 @@
 #ifndef _FILESYS_DATA_STORAGE_H
 #define _FILESYS_DATA_STORAGE_H
 
-#include <queue>
 #include <optional>
+#include <queue>
 #include <unordered_map>
 
 #include "file.h"
@@ -10,7 +10,6 @@
 class DataStorage {
     std::mutex _mu;
     fs::path _storage_directory;
-    std::fstream _recent_data_log;
     std::unordered_map<std::string, std::shared_ptr<File>> _file_list;
     const uint32_t _block_size;
 
@@ -22,9 +21,12 @@ class DataStorage {
                    uint32_t block_idx, const Bytes& block_data);
     Bytes ReadFile(const std::string& file_name, uint64_t stripe_offset,
                    uint64_t num_stripe, int32_t version);
-    std::optional<UpdateMetadata> GetLatestVersion(const std::string& file_name);
-    std::vector<std::shared_ptr<File>> GetFileList(const std::string& file_name);
-    bool DeleteFile(const std::string& file_name, uint32_t version, const Bytes& signature);
+    std::optional<UpdateMetadata> GetLatestVersion(
+        const std::string& file_name);
+    std::vector<std::shared_ptr<File>> GetFileList(
+        const std::string& file_name);
+    bool DeleteFile(const std::string& file_name, uint32_t version,
+                    const Bytes& signature);
     std::shared_ptr<File> operator[](const std::string& file_name);
 };
 
